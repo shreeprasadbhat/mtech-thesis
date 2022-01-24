b0d457315cbe458db4780ca29ef35858f22256d0
Shreeprasad Bhat <shreeprasadbt999@gmail.com>
2022-01-24 14:41:25 +0000

    updates

--- project-2/src/data/toy_models/TrueModel.py
+++ project-2/src/data/toy_models/TrueModel.py
@@ -2,22 +2,31 @@ import numpy as np
 
 class TrueModel:
     def __init__(self):
-        self.A_mean = -4
+        self.A_mean = -3.5
         self.A_err = 0.1
-        self.B_mean = 0
-        self.B_err = 0.01
-        self.C_mean = 0
+        self.B_mean = 3.6 
+        self.B_err = 0.1
+        self.C_mean = -0.1 
         self.C_err = 0.1
 
-    def out(self, z):
-        return -3.5 * z**2 + 3.6 * z - 0.1
+    def out(self, z, A=None, B=None, C=None):
+        if A is None:
+            A = self.A_mean
+        if B is None:
+            B = self.B_mean
+        if C is None:
+            C = self.C_mean
+        return A * z**2 + B * z + C
 
-    def err(self, size):
-        return np.random.normal(0, 1, size) 
+    def sample(self, z, size=(1,), A=None, B=None, C=None):
+        if A is None:
+            A = np.random.normal(self.A_mean, self.A_err, size)
+        if B is None:
+            B = np.random.normal(self.B_mean, self.B_err, size)
+        if C is None:
+            C = np.random.normal(self.C_mean, self.C_err, size)
+        return self.out(z, A, B, C)
 
-    def sample(self, z, size):
-        return out(z) + err(size)  
-    
 if __name__ == "__main__":
     obj = TrueModel()
     z = np.random.uniform(0,1,580)
