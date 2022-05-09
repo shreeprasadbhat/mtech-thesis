6d90bc2d9ae588ab6d99eb2b8f7f3c8a3601f8a2
Shreeprasad Bhat <shreeprasadbt999@gmail.com>
2022-05-09 17:39:04 +0000

    updates

--- project-3/data/train_test_split.py
+++ project-3/data/train_test_split.py
@@ -1,16 +1,16 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-filename = 'sky_data_model.txt'
+filename = 'Skyserver_Imaging5_9_2022 12_00_10 PM.csv'
 
-df = pd.read_csv(filename, header=None, sep=' ')
-df = df[df.iloc[:,-1] < 0.7]
-df = df[df.iloc[:,-1] > 0.4]
-df_train, df_test = train_test_split(df, test_size=0.2, random_state=123)
+df = pd.read_csv(filename, sep=',')
+#df = df[df.iloc[:,-1] < 0.7]
+#df = df[df.iloc[:,-1] > 0.4]
+df_train, df_test = train_test_split(df, test_size=0.3, random_state=123)
 
 print(df.shape)
 print(df_train.shape)
 print(df_test.shape)
 
-df_train.to_csv(filename[:-4]+'_filt1_train.txt', sep=' ', header=None, index=False)
-df_test.to_csv(filename[:-4]+'_filt1_test.txt', sep=' ', header=None, index=False)
+df_train.to_csv(filename[:-4]+'_train.csv', sep=' ', header=None, index=False)
+df_test.to_csv(filename[:-4]+'_test.csv', sep=' ', header=None, index=False)
